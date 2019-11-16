c42b74583543c04434d13019459ec21be30b1a37
Marcel <marcelkngtz@gmail.com>
2019-11-16 14:14:36 +0800

    work on post schema

---
+++ models/post.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const postsSchema = new mongoose.Schema({
+  poster: String,
+  title: String,
+  sub: String,
+  img: String,
+  content: String,
+  comments: Number,
+  likes: Number
+});
+
+// const good = mongoose.collection('goods', goodSchema);
+const post = mongoose.model("post", postsSchema);
+
+module.exports = post;
